a54b53be75e7d1459312ad296d0e51be27906468
Ben Clark <benjoeclark@gmail.com>
2009-11-24 13:11:25 -0600

    Added interface classes.
    Game taking form, being abstracted from displays.

--- mw10.py
+++ mw10.py
@@ -4,8 +4,7 @@ from mw10 import game
 from mw10 import display
 
 def main():
-    text_display = display.TextDisplay()
-    g = game.Game(text_display)
+    g = game.Game('console')
 
 if __name__ == '__main__':
     main()
