1809d2b05f3a17ded0548a1897a18c51baf0c15c
Ben Clark <benjoeclark@gmail.com>
2010-02-26 13:50:08 -0600

    added empty game and moved old code to tmp

---
+++ lib/game.py
@@ -0,0 +1,9 @@
+"""game.py
+
+This module starts and tracks the MW10 game"""
+
+class Game:
+    """Main game class"""
+    def __init__(self):
+        """Initialize and start the game"""
+        pass
