b4ea9e99a54ba6834b9cc2b277ab97fe7df4e703
Ben Clark <benjoeclark@gmail.com>
2010-02-26 15:32:50 -0600

    working with homegrown ui

--- lib/game.py
+++ lib/game.py
@@ -8,4 +8,19 @@ class Game:
     """Main game class"""
     def __init__(self):
         """Initialize and start the game"""
-        self.ui = ui.Ui()
+        self.log = open('log', 'w')
+        self.input = ui.Input()
+        self.screen = ui.Screen()
+        self.running = True
+        self.run()
+        self.log.close()
+
+    def run(self):
+        """Main game loop"""
+        while self.running:
+            key = self.input()
+            self.log.write(str(ord(key)) + '\n')
+            if ord(key) == 27:
+                self.running = False
+            self.screen.message(key)
+            self.screen.flip()
--- lib/ui.py
+++ lib/ui.py
@@ -2,13 +2,7 @@
 
 Module for handling user interface including keyboard and screen"""
 
-class Ui:
-    """Main user interface class"""
-    def __init__(self):
-        """Set up parts of the user interface"""
-        self.input = Input()
-        self.screen = Screen()
-
+import os
 
 class Input:
     """Class for handling user input"""
@@ -16,6 +10,10 @@ class Input:
         """Get the getch function"""
         import getch
         self.getch = getch.getch
+
+    def __call__(self):
+        """Call the getch function"""
+        return self.getch()
 
 
 class Screen:
@@ -25,4 +23,21 @@ class Screen:
     def __init__(self, width=80, height=24):
         """Initialize the screen with a width and height"""
         self.width = width
-        self.heigh = height
+        self.height = height
+        self.buffer = None
+        self.clear()
+        self.flip()
+
+    def clear(self):
+        """Clear the screen"""
+        self.buffer = ['*' * self.width] * self.height
+
+    def flip(self):
+        os.system('clear')
+        buffer_copy = self.buffer[:]
+        buffer_copy.reverse()
+        print '\n'.join(buffer_copy)
+
+    def message(self, string):
+        self.clear()
+        self.buffer[0] = string + self.buffer[0][len(string):]
