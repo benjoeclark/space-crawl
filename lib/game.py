b1eebb10312d7345a433e5a8337e00e8eb487dbb
Ben Clark <benjoeclark@gmail.com>
2010-03-01 18:51:47 -0600

    added first cut of save functionality

--- lib/game.py
+++ lib/game.py
@@ -4,6 +4,8 @@ This module starts and tracks the MW10 game"""
 
 import curses
 import state
+import space
+import cPickle
 
 class Game:
     """Main game class"""
@@ -16,9 +18,14 @@ class Game:
         self.running = True
         self.ui = screen
         self.screen = screen.subwin(23, 79, 0, 0)
+        try:
+            save_file = open('mw10.save', 'r')
+            self.universe = cPickle.load(save_file)
+        except:
+            self.universe = space.Universe()
         curses.raw()
         curses.curs_set(0) # make the cursor invisible
-        self.state = state.New(self.screen)
+        self.state = state.New(self.screen, self.universe)
         self.run()
 
     def run(self):
@@ -27,9 +34,15 @@ class Game:
             key = self.ui.getch()
             if key == 27:
                 self.running = False
+                self.save()
             else:
                 next_state = self.state.handle_key(key)
                 if next_state is not None:
                     self.state = next_state
             self.screen.addstr(22, 0, str(key))
             self.screen.refresh()
+
+    def save(self):
+        save_file = open('mw10.save', 'w')
+        cPickle.dump(self.universe, save_file)
+        save_file.close()
